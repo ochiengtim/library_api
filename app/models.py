0bf3146818246d1c7149abe7e441fe85f6f49e97
Timothy Ochieng Omondi <ochiengtim75@gmail.com>
2025-04-29 20:42:48 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from sqlalchemy import Column, Integer, String
+from app.db import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+    category_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), unique=True, nullable=False)
